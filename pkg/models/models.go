a6ef55c7402e836d8ff2489b113f1e5ff02ef066
preegnees <mrraziapov13@gmail.com>
2022-09-11 19:07:37 +0300

    перенес модели с репозитория: https://github.com/preegnees/SimpleMemStorageForGrpcServer. Добавил конфиг для постгреса, изменил конфига для клиента (добавил параметры авторизации)

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,10 +1,14 @@
 package models
 
 import (
+	"time"
+
 	pb "streaming/pkg/proto"
 )
 
-// CnfServer ...
+// ---> Conf
+
+// CnfServer. Конифгурация для сервера
 type CnfServer struct {
 	Addr            string
 	AuthToken       string
@@ -14,53 +18,86 @@ type CnfServer struct {
 	KeyPem          string
 }
 
-// CnfClient ...
+// CnfClient. Конфигурация для клиента
 type CnfClient struct {
 	Addr              string
-	AuthToken         string
 	RequestTimeout    int
 	KeepaliveInterval int
 	Reconnect         bool
 	ReconnectTimeout  int
+	IdChannel         string
+	Name              string
+	AllowedNames      string
 }
 
-// IServ ...
+// IServ. Интерфейс для сервера
 type IServ interface {
 	Run(CnfServer) error
 }
 
-// ICli ...
+// ICli. Интерфейс для клиента
 type ICli interface {
 	Run(CnfClient) error
 }
 
+// ---> MyMemStorage
+
+// IdChannel - канала, который связывет пиры
+type IdChannel string
+
+// Name - имя клиента
+type Name string
+
+// Token - токен для связки в хранилище каналы и пиры
+type Token string
+
+/*
+Peer. Пир - это подключение, которое имеет:
+IdChannel: id канала, с которым он связан;
+Name: имя, которое имеет пир;
+AllowedNames: разрешенные имена, с которым хочет соединится пир. Перечислять нужно через ","
+GrpcStream=стрим gprc;
+*/
 type Peer struct {
-	Name      string
-	Connected bool
-	Stream    pb.MyService_StreamingServer
+	IdChannel    IdChannel
+	Name         Name
+	AllowedNames string
+	GrpcStream   pb.MyService_StreamingServer
 }
 
-type Stream struct {
-	IdChannel string
-	Peers     []Peer
-}
-
-// IMemStorage ...
+/*
+IMemStorage. Интерфейс для взаимодействия с базой данных.
+SavePeer: сохранение пира;
+DeletePeer: удаление пира при отключении;
+*/
 type IStreamStorage interface {
-	SaveStream(strm Stream) (string, error)
-	SavePeer(idChannel string, peer Peer) error
-	SetFiledConnected(idChannel string, peer Peer, is bool) error
-	GetStreams() (strm chan<- Stream)
+	SavePeer(peer Peer) (<-chan map[Peer]struct{}, error)
+	DeletePeer(peer Peer) error
 }
 
-// IAccessStorage ...
-type IAccessStorage interface {
-	Check(string) (bool, error)
-	SaveNewName(string) (bool, error)
+// ---> Postgress
+
+type ConfPostgres struct {
+	TestMod  bool
+	User     string
+	Host     string
+	Password string
+	Port     string
+	Database string
+	Sslmode  string
 }
 
-// IStorage ...
-type IStorage interface {
-	IAccessStorage
-	IStreamStorage
+type Connection struct {
+	ID           uint `gorm:"primaryKey"`
+	IP           string
+	Name         Name
+	IdChannel    IdChannel
+	AllowedNames string
+	Time         time.Time
+}
+
+type IPostgres interface {
+	Save(Connection) error
+	GetConnsUseName(Name) ([]Connection, error)
+	GetConnsUseIdChannel(IdChannel) ([]Connection, error)
 }
