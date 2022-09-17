b6f29f4ee9be73a80bb299abfb4dab1eb8544e65
preegnees <mrraziapov13@gmail.com>
2022-09-17 13:10:58 +0300

    ВЫнес функцию PeerToStr в коммон

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,2 +1,14 @@
 package common
 
+import (
+	"fmt"
+	
+	m "streaming/pkg/models"
+)
+
+func PeertoString(peer m.Peer) string {
+	return fmt.Sprintf(
+		"Name=%s, IdChannel=%s, AllowedNames=%s, GrpcStream=%v",
+		peer.Name, peer.IdChannel, peer.AllowedNames, peer.GrpcStream,
+	)
+}
